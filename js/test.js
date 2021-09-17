b0da0dc998869a3cf974716a272358f468941492
Ben Trainor <ben.trainor@my.uu.edu>
2021-09-17 14:49:19 -0500

    Added Raleway font and Tempo Calculator

--- js/test.js
+++ js/test.js
@@ -1,6 +1,20 @@
-const { Howl } = require("howler");
 
-var sound = new Howl({
-    src: ["unknownfile.mp3"]
-});
-sound.play();
+function tempoCalc(tempo) {
+
+    var millisPerBeats = 0;
+
+    millisPerBeats = 60000 / tempo; // converts tempo to beats per millisecond
+    
+    console.log(tempo + " - Beat length in millis: " + millisPerBeats + "ms");
+    console.log(tempo + " - Length of one bar in 4/4: " + millisPerBeats * 4 + "ms");
+
+}
+
+var bpm;
+function tempoPrompt() {
+    
+    console.log("Creating text box...");
+    bpm = prompt("Enter BPM: ");
+    tempoCalc(bpm);
+    
+}
